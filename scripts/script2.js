d8ad513609d7ae472da3a316bda99a9fe39189c3
robynloudang <robyn.dang@hotmail.ca>
2022-04-25 17:37:15 -0400

    update

--- scripts/script2.js
+++ scripts/script2.js
@@ -63,3 +63,20 @@ $(window).scroll(function() {
       percent   = 110 - ((110*scrollTop)/hHeight) * 2;
       $(".flex-case-2").css("border-top-right-radius, border-top-left-radius", percent + "%");
 });
+
+/*CONTACT FORM*/
+function sendEmail(){
+  Email.send({
+  Host : "smtp-relay.sendinblue.com",
+  Username : "robyn.lou.dang@gmail.com",
+  Password : "hJt1BaO25FGPZXVk",
+  To : 'robyn.lou.dang@gmail.com',
+  From : document.getElementById("email").value,
+  Subject : "New Contact Form Enquiry",
+  Body : "Name: " + document.getElementById("name").value
+    + "<br> Email: " + document.getElementById("email").value
+    + "<br> Message: " + document.getElementById("message").value
+}).then(
+message => alert("Message sent successfully")
+);
+}
